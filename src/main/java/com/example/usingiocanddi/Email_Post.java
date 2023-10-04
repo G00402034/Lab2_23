452da3a40128c094c662ac50ba6b7fd957cce61c
G00402034 <G00402034@atu.ie>
2023-10-04 15:57:23 +0100

    finished user post  class

--- src/main/java/com/example/usingiocanddi/Email_Post.java
+++ src/main/java/com/example/usingiocanddi/Email_Post.java
@@ -1,4 +1,18 @@
 package com.example.usingiocanddi;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Email_Post {
+    @PostMapping("registerUserBody")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registerUserCredential(@RequestBody User user) {
+        System.out.println("User ID: " + user.getUserName());
+        System.out.println("User ID: " + user.getUserEmail());
+        return user;
+    }
 }
