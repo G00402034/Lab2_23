ab0abb2760abf422b600a7fefbee63cbe43e67dd
G00402034 <g00402034@atu.ie>
2023-10-04 15:59:52 +0100

    finished user post  class

---
+++ src/main/java/com/example/usingiocanddi/UsingIoCandDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.usingiocanddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingIoCandDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingIoCandDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usingiocanddi/UsingIoCandDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usingiocanddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingIoCandDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
